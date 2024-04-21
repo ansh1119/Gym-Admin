803429b15f14a4a30ff8f440b95673d82163ffe2
ajeeshrawal <ajeeshrawal3376@gmail.com>
2024-04-22 02:48:38 +0530

    DueMemberScreen Added

---
+++ app/src/main/java/com/example/gymadmin/Item.kt
@@ -0,0 +1,11 @@
+package com.example.gymadmin
+
+import java.text.DateFormat
+
+data class Item(
+    var gender:String,
+    var name:String,
+    var number:Long,
+    var duration: Int,
+    var date:String
+)
--- app/src/main/java/com/example/gymadmin/MainActivity.kt
+++ app/src/main/java/com/example/gymadmin/MainActivity.kt
@@ -7,6 +7,53 @@ import androidx.activity.enableEdgeToEdge
 
 
 class MainActivity : ComponentActivity() {
+
+    companion object{
+        var items= listOf(
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+            Item(
+                gender = "male",
+                name = "Ajeesh Rawal",
+                number = 7393085376,
+                duration = 3,
+                date = "8/9/2023"
+            ),
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
